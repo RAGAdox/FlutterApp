750f15986c2c1c1a1a17c36208c080823e592d35
RAGAdox <rishitech882@gmail.com>
2019-10-30 23:17:46 +0530

    Hot Reload Enabled || Implemented stateless wigets

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,36 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('RAGAdox'),
-      centerTitle: true,
-      backgroundColor: Colors.red[600],
-    ),
-    body: Center(
-      child: Text(
-          'Hey Bois',
-      style: TextStyle(
-        fontSize: 20.0,
-        fontWeight: FontWeight.bold,
-        letterSpacing: 2.0,
-        color: Colors.grey[600],
-        fontFamily: 'Rubik',
-      ),),
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: (){},
-      child: Text('Press me Hard !!'),
-      backgroundColor: Colors.red[600],
-    ),
-  ),
+  home:Home(),
 ));
 
+class Home extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('RAGAdox'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center(
+        child: Text(
+          'Hey Bois',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey[600],
+            fontFamily: 'Rubik',
+          ),),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child: Text('Yo !!'),
+        backgroundColor: Colors.red[600],
+      ),
+    );
+  }
 
+}
